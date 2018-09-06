283c2362fec5f5386309fdc3eb3ac070f69f43d5
Phil Nash <philnash@gmail.com>
2018-09-06 15:24:48 +1000

    Get microphone access

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
 import React, { Component } from 'react';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      audio: null
+    };
+    this.getMicrophone = this.getMicrophone.bind(this);
+    this.stopMicrophone = this.stopMicrophone.bind(this);
+  }
+
+  getMicrophone() {
+    navigator.mediaDevices
+      .getUserMedia({ audio: true, video: false })
+      .then(audio => this.setState({ audio }));
+  }
+
+  stopMicrophone() {
+    this.state.audio.getTracks().forEach(track => track.stop());
+    this.setState({ audio: null });
+  }
+
   render() {
     return (
       <div className="App">
         <main>
-          <div class="controls">
-            <button>Get microphone input</button>
+          <div className="controls">
+            <button
+              onClick={
+                this.state.audio ? this.stopMicrophone : this.getMicrophone
+              }
+            >
+              {this.state.audio ? 'Stop microphone' : 'Get microphone input'}
+            </button>
           </div>
         </main>
       </div>
